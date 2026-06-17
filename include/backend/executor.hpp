7cf42480a7dafe6657e0d8bd674e1f004c1eb103
cruelkratos <mailgarvsethi@gmail.com>
2026-06-18 01:37:26 +0530

    executor and insert operation

--- include/backend/executor.hpp
+++ include/backend/executor.hpp
@@ -1,13 +1,39 @@
+#pragma once
 #include<include/frontend/parser/AST.hpp>
 #include<memory>
+#include<optional>
+#include<memory_manager.hpp>
+#include<vector>
+#include<cstring>
+#include<table_manager.hpp>
+
 
 namespace db::executor{
 	class AbstractExecutor{
-		private:
+		protected:
 		std::unique_ptr<AbstractExecutor> _child;
+		db::parser::ASTNode* node;
 		public:
 		virtual void init() = 0;
-		virtual void next() = 0; // won't be void just putting for now
+		virtual std::optional<db::memory::Tuple> next() = 0; 
 		virtual ~AbstractExecutor();
 	};
+
+	class InsertOperator : public AbstractExecutor{
+		//iterate over the values in the node passed to the executor.
+		//convert lexeme to actual type.
+		//create required bytes
+		//do std::memcpy
+		//push into a buffer
+		public:
+		std::optional<db::memory::Tuple> next() override;
+		private:
+		db::memory::Tuple serializeToTuple();
+		bool _hasInserted = false;
+		db::memory::TableManager* table_manager;
+		
+		
+	};
+
+
 }
---
+++ src/backend/insert_operator.cpp
@@ -0,0 +1,64 @@
+#include<executor.hpp>
+#include<stdexcept>
+#include<algorithm>
+#include<iostream>
+
+namespace db::executor{
+	// InsertOperator::
+	db::memory::Tuple InsertOperator::serializeToTuple(){
+		auto* node_ = static_cast<db::parser::InsertStatement*>(node);
+		std::vector<char> buffer;
+
+		for(auto &v : node_->values){
+			if(db::table::tokenToColumnType(v.type) == db::table::ColumnType::INT){
+				int val = std::stoi(v.lexeme);
+				char raw_bytes[sizeof(int)];
+
+				std::memcpy(raw_bytes,&val,sizeof(int));
+				buffer.insert(buffer.end(),raw_bytes,raw_bytes+sizeof(int));
+			}
+
+			else if(db::table::tokenToColumnType(v.type) == db::table::ColumnType::BOOL){
+				bool a = false;
+				if(v.type == db::lexer::TokenType::TRUE){
+					a = true;
+				}
+				char raw_bytes[sizeof(bool)];
+				std::memcpy(raw_bytes,&a,sizeof(bool));
+				buffer.insert(buffer.end(),raw_bytes,raw_bytes+sizeof(bool));
+			}
+
+			else if(db::table::tokenToColumnType(v.type) == db::table::ColumnType::TEXT){
+				auto str_val = v.lexeme;
+				char text_bytes[255] = {0};
+				size_t copy_length = std::min(int(str_val.length()), 255 - 1);
+    			std::memcpy(text_bytes, str_val.c_str(), copy_length);
+				buffer.insert(buffer.end(),text_bytes,text_bytes+255);
+			}
+			else{
+				throw std::runtime_error("EXECUTION ERROR: Unsupported Column type for insertion.");
+			}
+		}
+
+		return db::memory::Tuple(buffer);
+	}
+
+	std::optional<db::memory::Tuple> InsertOperator::next(){
+		if(_hasInserted){
+			return std::nullopt;
+		}
+
+		db::memory::Tuple physical_tuple = serializeToTuple();
+		try{
+			table_manager->createTuple(physical_tuple);
+		}
+		catch(const std::runtime_error& e){
+			std::cerr<<"EXECUTOR ERROR: CANT ADD TUPLE"<<std::endl;
+			throw e;
+		}
+
+		_hasInserted = true;
+		return physical_tuple;
+
+	}
+}
